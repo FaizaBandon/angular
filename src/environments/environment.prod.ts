98b3003f81de2039867a152dc25cb405da38d7b0
nktaher <norhaintaher@gmail.com>
2022-06-04 07:04:26 -0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://faizabandon-001-site1.itempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false
+  },
+  apis: {
+    default: {
+      url: 'http://faizabandon-001-site1.itempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
