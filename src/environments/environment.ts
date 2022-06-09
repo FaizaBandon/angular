5033ab28f2148d21e07a7eb87038e46b56b14c60
Bai Faiza <91953953+FaizaBandon@users.noreply.github.com>
2022-06-10 02:27:01 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://bandon-akmad-taher.netlify.app';
+const baseUrl = 'https://faiza.netlify.app';
 
 export const environment = {
   production: true,
